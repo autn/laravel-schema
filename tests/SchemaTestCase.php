bf420e8caf9fe44ec166488e1cc8f511419fc61f
autn <autn@greenglobal.vn>
2016-05-17 16:57:35 +0700

    fix(Tests): Add tests file
    Add tests file

--- tests/SchemaTestCase.php
+++ tests/SchemaTestCase.php
@@ -19,7 +19,7 @@ class SchemaTestCase extends Illuminate\Foundation\Testing\TestCase
      */
     public function createApplication()
     {
-        $app = require __DIR__.'../bootstrap/app.php';
+        $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
